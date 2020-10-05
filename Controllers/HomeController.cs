4945919d2496e329368cd162f5ad3873c47ae964
Master <polayohanna5@gmail.com>
2020-10-05 17:28:22 +0200

    Third Commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.Logging;
@@ -56,6 +57,25 @@ namespace ProjectA_Movies.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
         [HttpPost]
+        public IActionResult Search(string name, string actionpage)
+        {
+            
+            return actionpage == "" ? RedirectToAction("Index") : RedirectToAction(actionpage, new { name });
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Movie movie = await _context.Movies.FirstOrDefaultAsync(e=>e.Id==id);
+            if (movie==null)
+            {
+                return BadRequest();
+            }    
+            _context.Movies.Remove(movie);
+            await _context.SaveChangesAsync();
+            return View("Index");
+        }
+        [HttpGet]
         public async Task<IActionResult> Delete(string name)
         {
             Movie movie = await _context.Movies.FirstOrDefaultAsync(e => e.Name == name);
@@ -64,6 +84,29 @@ namespace ProjectA_Movies.Controllers
                 return NotFound();
             }
             return View(movie);
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Edit(string name)
+        {
+            Movie movie = await _context.Movies.FirstOrDefaultAsync(e => e.Name == name);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public async Task<IActionResult> Edit(Movie EditedMovie)
+        {
+            if(EditedMovie==null)
+            {
+                return BadRequest();
+            }
+            _context.Movies.Update(EditedMovie);
+            await _context.SaveChangesAsync();
+            return View("Index");
         }
     }
 }
